d35f5bb8d4993e24c49d2c1661cf5fbc50973cbe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-30 10:53:27 +0300

    Added some error checking logic

--- routers/cars.py
+++ routers/cars.py
@@ -2,7 +2,7 @@
 from typing import List
 
 from beanie import PydanticObjectId
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from models.models import Car
 
@@ -20,11 +20,22 @@ async def get_all_second_hand_cars() -> List[Car]:
 async def get_single_car_by_id(car_id: PydanticObjectId) -> Car:
     """The endpoint to retrieve a single car by id"""
     car = await Car.find_one(car_id)
+
+    if not car:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Car entry not found"
+        )
+
     return car
 
 
-@cars_router.post("/")
+@cars_router.post("/", status_code=status.HTTP_201_CREATED, )
 async def create_car(car: Car) -> Car:
     """The endpoint to create a car"""
-    await car.create()
-    return car
+    try:
+        await car.create()
+        return car
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Car entry already exists")
