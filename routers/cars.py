8e148d1f36fac5fe8130aef4d52af3588c4dc09e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-30 10:45:19 +0300

    Wrote the endpoint to retrieve one car

--- routers/cars.py
+++ routers/cars.py
@@ -4,6 +4,7 @@ from typing import List
 from fastapi import APIRouter
 
 from models.models import Car
+from beanie import PydanticObjectId
 
 cars_router = APIRouter(prefix="/cars", tags=["Cars"])
 
@@ -13,3 +14,10 @@ async def get_all_second_hand_cars() -> List[Car]:
     """The endpoint to retrieve all the cars from the database"""
     cars = await Car.find_all().to_list()
     return cars
+
+
+@cars_router.get("/{car_id}")
+async def get_single_car_by_id(car_id: PydanticObjectId) -> Car:
+    """The endpoint to retrieve a single car by id"""
+    car = await Car.find_one(car_id)
+    return car
