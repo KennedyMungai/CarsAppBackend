95b23ceb3c41f1d4b52a7e567162be80c1f48eb1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-30 10:38:23 +0300

    Wrote the endpoint to retrieve all cars

--- routers/cars.py
+++ routers/cars.py
@@ -11,5 +11,5 @@ cars_router = APIRouter(prefix="/cars", tags=["Cars"])
 @cars_router.get("/")
 async def get_all_second_hand_cars() -> List[Car]:
     """The endpoint to retrieve all the cars from the database"""
-    cars = await Car.all()
+    cars = await Car.find_all().to_list()
     return cars
