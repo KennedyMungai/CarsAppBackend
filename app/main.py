a1f6499ccbe9beccad2aaed272f392356110b6c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-30 10:27:27 +0300

    Added a function that runs on event startup

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 """The main entrypoint for the application"""
 from fastapi import FastAPI
+from database.db import init_db
 
 app = FastAPI(
     title="Second Hand Cars",
     description="The backend of a second hand cars app",
     version="0.1.0"
 )
+
+
+@app.on_event("startup")
+async def database_connection():
+    """The function to connect to the database on startup"""
+    print("Connecting to database...")
+    await init_db()
 
 
 @app.get("/")
