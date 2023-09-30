75d005f2dbdb4b53d7ce3c583d60c97dd4ed3756
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-30 10:20:15 +0300

    Defined the models module

---
+++ models/models.py
@@ -0,0 +1,37 @@
+"""The models for the application"""
+from typing import Optional
+
+from beanie import Document
+from pydantic import BaseModel
+
+
+class Car(Document):
+    """The car model"""
+    brand: str
+    make: str
+    year: int
+    price: int
+    km: int
+    cm3: int
+
+    class Settings:
+        """The settings for the car model"""
+        name = "cars"
+
+    class Config:
+        """The config for the car model"""
+        json_schema_extra = {
+            "example": {
+                "brand": "Mercedes",
+                "make": "Some pretentious words",
+                "year": 1999,
+                "price": 999999,
+                "km": 999999,
+                "cm3": 999999
+            }
+        }
+
+
+class CarUpdate(BaseModel):
+    """The car update model"""
+    price: Optional[int] = None
